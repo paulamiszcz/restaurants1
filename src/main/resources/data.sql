7f3fbab2fd9d06a295f1ffbf122f9a9c2232750a
Marek Procek <marekprocek@gmail.com>
2018-08-14 19:21:59 +0200

    Potrawy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
+INSERT INTO meal(name, price) VALUES ('Rolada, kluski śląskie, modro kapusta', 29);
 INSERT INTO city(id, name, longitude, latitude) VALUES (1, 'Wrocław', 51.06, 17.01);
 INSERT INTO city(id, name, longitude, latitude) VALUES (2, 'Warszawa', 52.13, 21.00);
