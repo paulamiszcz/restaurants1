8f934a8b871849bec82fa18ad2bf9e9b1620bd95
Marcin <mlukasik93@gmail.com>
2018-08-14 19:18:25 +0200

    dodano stronę glowna

--- src/main/java/pl/javastart/dobrex/MainController.java
+++ src/main/java/pl/javastart/dobrex/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-            public String home (){
+    public String home() {
         return "home";
-            }
+    }
 }
