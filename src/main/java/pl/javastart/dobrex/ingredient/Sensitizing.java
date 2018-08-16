b206a88ebc04f9c5093af4b55e4ef6b02ab4c605
EwelinaGordon <ewelina.gordon@gmail.com>
2018-08-16 21:47:03 +0200

    Poprawka do enuma

--- src/main/java/pl/javastart/dobrex/ingredient/Sensitizing.java
+++ src/main/java/pl/javastart/dobrex/ingredient/Sensitizing.java
@@ -1,6 +1,16 @@
 package pl.javastart.dobrex.ingredient;
 
 public enum  Sensitizing {
-    TAK,
-    NIE;
+    YES("TAK"),
+    NO("NIE");
+
+   private String description;
+
+    Sensitizing(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO meal(name, price) VALUES ('Rolada, kluski śląskie, modro kapusta', 29);
 INSERT INTO city(id, name, longitude, latitude) VALUES (1, 'Wrocław', 51.06, 17.01);
 INSERT INTO city(id, name, longitude, latitude) VALUES (2, 'Warszawa', 52.13, 21.00);
-INSERT INTO ingredient(name, price, is_sensitizing) VALUES ('Pieczarki', 21.19, 'TAK');
-INSERT INTO ingredient(name, price, is_sensitizing) VALUES ('Bułka', 0.19, 'NIE');
-INSERT INTO ingredient(name, price, is_sensitizing) VALUES ('Pomidory', 8.00, 'TAK');
+INSERT INTO ingredient(name, price, is_sensitizing) VALUES ('Pieczarki', 21.19, 'YES');
+INSERT INTO ingredient(name, price, is_sensitizing) VALUES ('Bułka', 0.19, 'NO');
+INSERT INTO ingredient(name, price, is_sensitizing) VALUES ('Pomidory', 8.00, 'YES');
